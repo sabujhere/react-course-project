c024d1423fb2d06cd07fe11f835d6a3c60985167
Sabuj_S&C <sabuj.saha@sandc.com>
2020-07-26 19:24:00 -0500

    exploring jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,59 @@
 'use strict';
 
 console.log('App.js is running');
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Does this change'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' Sabuj Saha'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age 37'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Chicago'
+    )
+);
+
 var appRoot = document.getElementById('app');
+var appRoot2 = document.getElementById('personalInfo');
 
 ReactDOM.render(template, appRoot);
+
+ReactDOM.render(template2, appRoot2);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,27 @@
 console.log('App.js is running')
- var template = <h1>Does this change</h1>
-var appRoot = document.getElementById('app')
+
+var template = ( 
+<div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>
+);
+
+var template2 = (
+    <div>
+        <h1> Sabuj Saha</h1>
+        <p>Age 37</p>
+        <p>Location: Chicago</p>
+    </div>
+)
+
+var appRoot = document.getElementById('app');
+var appRoot2 = document.getElementById('personalInfo')
 
 ReactDOM.render(template, appRoot);
+
+ReactDOM.render(template2, appRoot2)
