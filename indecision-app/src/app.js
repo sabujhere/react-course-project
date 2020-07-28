ef9f06e9564d39efc04dfee93e6014b76e5f0474
Sabuj_S&C <sabuj.saha@sandc.com>
2020-07-28 15:28:28 -0500

    nest component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -5,7 +18,7 @@ class Header extends React.Component {
         <h1>Indecision</h1>
         <h2>Put your life in the hands of a computer</h2>
       </div>
-    )
+    );
   }
 }
 
@@ -15,35 +28,39 @@ class Action extends React.Component {
       <div>
         <button>What should I do?</button>
       </div>
-    )
+    );
   }
 }
+
 class Options extends React.Component {
   render() {
     return (
       <div>
-      <p> Options Component here!</p>
+        Options component here
+        <Option />
       </div>
-    )
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option component here
+      </div>
+    );
   }
 }
 
 class AddOption extends React.Component {
-  render(){
+  render() {
     return (
       <div>
-        <p>AddOption Component here!</p>
+        AddOption component here
       </div>
-    )
+    );
   }
 }
-const jsx = (
-  <div>
-  <Header />
-  <Action />
-  <Options />
-  <AddOption />
-  </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
