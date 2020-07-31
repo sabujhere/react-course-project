462796d5c5c7ed0146b8e41d950cb96dc54c4d2d
Sabuj_S&C <sabuj.saha@sandc.com>
2020-07-31 15:19:10 -0500

    es6 class properties

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
+  state = {
+    error: undefined
+  };
   constructor(props) {
     super(props);
     this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    };
   }
-  handleAddOption(e) {
+  handleAddOption = (e) => {
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
@@ -18,7 +18,8 @@ export default class AddOption extends React.Component {
     if (!error) {
       e.target.elements.option.value = '';
     }
-  }
+  };
+  
   render() {
     return (
       <div>
