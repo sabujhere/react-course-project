93c4f61828030d454553742a2fd95c5489544895
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-04 21:30:26 -0500

    subscribing and dynamic acitons

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -4,12 +4,14 @@ const store = createStore((state = { count: 0}, action) =>{
 
     switch(action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + 1 
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - 1 
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
@@ -20,14 +22,21 @@ const store = createStore((state = { count: 0}, action) =>{
     }
 });
 
-console.log(store.getState())
-
-store.dispatch({
-    type:'INCREMENT'
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState())
 });
 
+
 store.dispatch({
-    type:'INCREMENT'
+    type:'INCREMENT',
+    incrementBy: 5
+});
+
+
+
+store.dispatch({
+    type:'DECREMENT',
+    decrementBy: 10
 });
 
 store.dispatch({
@@ -38,5 +47,3 @@ store.dispatch({
 store.dispatch({
     type:'DECREMENT'
 });
-
-console.log(store.getState())
