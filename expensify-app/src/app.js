557c8a864e50573bec25cf2bedecc925bbcbe956
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-10 22:00:51 -0500

    connecting store and component with react redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import configureStore from  './store/configureStore';
-import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({ description: 'Water bill', amount: 400, createdAt: 1000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee bill', amount: 300, createdAt: -1000 }));
-store.dispatch(setTextFilter('coffee'));
-const state = store.getState()
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
+const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
