073d2e4abb39ea4e09c4ba4ae98d4229844f66b6
Sabuj_S&C <sabuj.saha@sandc.com>
2020-08-20 15:06:15 -0500

    fixed bulid issue

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/
     }, {
-      test:/\.s?css$/,
+      test: /\.s?css$/,
       use: [
         'style-loader',
         'css-loader',
@@ -23,6 +23,6 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback:true
-  }  
+    historyApiFallback: true
+  }
 };
