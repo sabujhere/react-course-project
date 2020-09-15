9b510a43e271d0bf169a6991e6adaf62945b87c6
Sabuj_S&C <sabuj.saha@sandc.com>
2020-09-15 17:08:33 -0500

    context api part 1

---
+++ react-new-features/src/components/Note.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+const Note = ({ note, removeNote }) => {
+    useEffect(() => {
+        console.log('Setting up effect!')
+
+        return () => {
+            console.log('Cleaning up effect!')
+        }
+    }, [])
+
+    return (
+        <div>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+    )
+}
+
+export {Note as default}
---
+++ react-new-features/src/components/NoteApp.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useReducer } from 'react';
+import notesReducer from '../reducers/notes';
+import NoteList from './NoteList';
+import AddNoteForm from './AddNoteForm';
+const NoteApp = () => {
+    const [notes, dispatch] = useReducer(notesReducer, [])
+    const removeNote = (title) => {
+        dispatch({
+            type: 'REMOVE_NOTE',
+            title
+        })
+    }
+
+    useEffect(() => {
+        const notes = JSON.parse(localStorage.getItem('notes'))
+
+        if (notes) {
+            dispatch({ type: 'POPULATE_NOTES', notes })
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <NoteList notes={notes} removeNote={removeNote} />
+            <AddNoteForm dispatchAddNote={dispatch}/>
+        </div>
+    )
+}
+
+export {NoteApp as default}
