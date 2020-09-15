2086c584ad0d70e11a9f2349c3b9bf4c1c3c670e
Sabuj_S&C <sabuj.saha@sandc.com>
2020-09-15 10:21:14 -0500

    the use reducer hook

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,102 +1,100 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+//
+// Goal: Setup support for adding and removing notes
+//
+// 1. Setup and dispatch an ADD_NOTE action
+// 2. Setup and dispatch a REMOVE_NOTE action
+// 3. Test your work!
 
- const NoteApp = () => {
-//   const noteData = JSON.parse(localStorage.getItem('notes'))
-  const [notes, updateNotes] = useState([])
-  const [title, updateTitle] = useState('')
-  const [body, updateBody] = useState('')
-
-  const addNote = (e) =>{
-    e.preventDefault()
-    updateNotes([
-      ...notes,
-      {
-        title, 
-        body
-      }
-    ])
-    updateTitle('')
-    updateBody('')
-  }
-
-  const removeNote = (title) =>{
-    updateNotes(notes.filter((note) => note.title !== title))
-  }
-
-  useEffect(()=>{
-    const noteData = JSON.parse(localStorage.getItem('notes'))
-    if(noteData) {
-      updateNotes(noteData)
+const notesReducer = (state, action) => {
+    switch (action.type) {
+        case 'POPULATE_NOTES':
+            return action.notes
+        case 'ADD_NOTE':
+            return [
+                ...state,
+                { title: action.title, body: action.body }
+            ]
+        case 'REMOVE_NOTE':
+            return state.filter((note) => note.title !== action.title )
+        default:
+            return state
     }
-  }, [])
-
-
-  useEffect(()=>{
-    localStorage.setItem('notes', JSON.stringify(notes))
-  },[notes])
-  
-  return (
-    <div>
-      <h1>Notes</h1>
-      {notes.map((note) =>(
-       <Note key={note.title} note={note} removeNote={removeNote}/>
-      ))}
-      <p>Add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => updateTitle(e.target.value) }/>
-        <textarea value={body} onChange={(e) =>updateBody(e.target.value)}/>
-        <button>Add note</button>
-      </form>
-    </div>
-  )
 }
 
+const NoteApp = () => {
+    const [notes, dispatch] = useReducer(notesReducer, [])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
-const Note = ({note, removeNote}) =>{
-  useEffect(()=>{
-    console.log('Setting up effect!')
-
-    return ()=>{
-      console.log('Cleaning up effect!')
+    const addNote = (e) => {
+        e.preventDefault()
+        dispatch({
+            type: 'ADD_NOTE',
+            title,
+            body
+        })
+        setTitle('')
+        setBody('')
     }
-  }, [])
-  return (
-    <div >
-      <h3>{note.title}</h3>     
-      <p>{note.body}</p>     
-      <button onClick={() =>removeNote(note.title)}>X</button>
-    </div>
-  )
+
+    const removeNote = (title) => {
+        dispatch({
+            type: 'REMOVE_NOTE',
+            title
+        })
+    }
+
+    useEffect(() => {
+        const notes = JSON.parse(localStorage.getItem('notes'))
+
+        if (notes) {
+            dispatch({ type: 'POPULATE_NOTES', notes })
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            {notes.map((note) => (
+                <Note key={note.title} note={note} removeNote={removeNote}/>
+            ))}
+            <p>Add note</p>
+            <form onSubmit={addNote}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <button>add note</button>
+            </form>
+        </div>
+    )
 }
-// const App = (props)=> {
-//   const [count, updateCount] = useState(props.count)
-//   const [text, updateText] = useState('test')
 
-//   useEffect(() =>{
-//     console.log('useEffect only run once')
-//   }, [])
+const Note = ({ note, removeNote }) => {
+    useEffect(() => {
+        console.log('Setting up effect!')
 
-//   useEffect(() =>{
-//     console.log('useEffect ran')
-//     document.title = count
-//   }, [count])
-//   return (
-//     <div>
-//       <p>This current {text || 'count'} is {count}</p>
-//       <button onClick={()=>updateCount(count + 1)}>+1</button>
-//       <button onClick={()=>updateCount(props.count)}>Reset</button>
-//       <button onClick={()=>updateCount(count - 1)}>-1</button>
-//       <input onChange={(e)=>updateText(e.target.value)} value={text}/>
-//     </div>
-//   )
-// }
+        return () => {
+            console.log('Cleaning up effect!')
+        }
+    }, [])
 
-ReactDOM.render(<NoteApp />,
-  document.getElementById('root')
-);
+    return (
+        <div>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<NoteApp/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
