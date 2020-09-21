4b8646f75873f8e5d18c8e1d004849f5faf8c85d
Sabuj_S&C <sabuj.saha@sandc.com>
2020-09-21 12:40:35 -0500

    added loading state with useState

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,25 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
 function App() {
-
   const [results, setResults] = useState([])
+  const [query, setQuery] = useState('reacthooks')
+  const searchInputRef = useRef();
+  const [loading, setLoading] = useState(false);
 
   useEffect( ()=>{
     getResults();
-    // .then(response=>{
-    //   console.log(response.data);
-    //   setResult(response.data.hits)
-    //})
   },[])
 
   const getResults = async () =>{
-    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+    setLoading(true);
+    const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
+    setLoading(false);
   };
+
+  const handleSearch = event =>{
+    event.preventDefault();
+    getResults()
+  }
+
+  const handleClearSearch = () =>{
+    setQuery("");
+    searchInputRef.current.focus();
+  }
+
   return (
     <>
-    <ul>
+    <form onSubmit={handleSearch}>
+      <input 
+        type="text" 
+        value={query} 
+        onChange={(e)=>setQuery(e.target.value)}
+        ref={searchInputRef}/>
+      <button type="submit">Search</button>
+      <button type="button" onClick={handleClearSearch}>Clear</button>
+    </form>
+    
+    { loading ? (
+      <div>
+      Loading
+      </div>
+    ):
+      (
+        <ul>
     {
       results.map(result =>(
         <li key={result.objectID}>
@@ -28,6 +55,7 @@ function App() {
       ))
     }
     </ul>
+    )}
     </>
   );
 }
